2bcf443d6ff08917348cf8a8d782972f22e4e8f8
nahyun <91935160+nahyunme@users.noreply.github.com>
2021-10-26 14:34:11 +0900

    Add files via upload

---
+++ doc/slick01.js
@@ -0,0 +1,16 @@
+$(function(){
+//--------------------------------
+$('.main_slider').slick({
+    arrows:false,
+    autoplay:true,
+    pauseOnHover:false,
+    //vertical:true - 아래에서 위로 올라옴 
+    fade:true, - 흐려졌다가 나옴 //
+});
+
+
+
+
+
+//----------------------------------
+})
