36e020e3b14126d486346fe04db71646636988d7
nahyun <91935160+nahyunme@users.noreply.github.com>
2021-11-08 12:55:26 +0900

    Add files via upload

---
+++ doc/responsive01.js
@@ -0,0 +1,12 @@
+$ (function(){
+
+    $('#header i').on('click',function(){
+        $('nav').toggle();
+    });
+
+    $(window).on('resize',function(){
+        $('nav').removeAttr('style');
+    });
+
+
+});
