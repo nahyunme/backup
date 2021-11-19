b9b53893349cdba3ed57e458ab8a0f5185299218
nahyun <91935160+nahyunme@users.noreply.github.com>
2021-11-19 11:17:23 +0900

    Add files via upload

---
+++ doc/gunu01.js
@@ -0,0 +1,13 @@
+$(function(){
+//------------------------------
+$(".main_slider").owlCarousel({
+    items:1,
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    nav:true,
+    dots:true,
+});
+
+//----------------------------------
+})
