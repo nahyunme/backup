edb7c880b4a6b86aa4f43c9da30d01343cb109f1
nahyun <91935160+nahyunme@users.noreply.github.com>
2021-11-25 20:28:38 +0900

    Add files via upload

--- pf03/js/pf03.js
+++ pf03/js/pf03.js
@@ -18,6 +18,20 @@ $ (function(){
     });
 
     
-    //$('#section01 .container').slick//
+    //$('#section01 .item01').slick//
+
+    $('.cn_slider').slick({
+        arrows:false,
+        slidesToShow:3,
+        autoplay:true,
+        autoplaySpeed:2000,
+        pauseOnHover:false,
+        pauseOnFocus:false,
+    });
+    $('.cn_slider figure').eq(4).addClass('on');
+    $('.cn_slider').on('afterChange', function(e,s,c){
+        $('.cn_slider figure').eq(c+4).addClass('on').siblings().removeClass('on');
+        $('#section02 .slide_bar>span').css({left:c*69.2})
+    });
     
 })
