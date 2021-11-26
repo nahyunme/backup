35bfd8e968a2ffccfe93eb0571df240cab273274
nahyun <91935160+nahyunme@users.noreply.github.com>
2021-11-26 11:15:40 +0900

    Add files via upload

--- trn01/layout.js
+++ trn01/layout.js
@@ -1,40 +1,34 @@
 $(function(){
-//------------------------
 
-// $(window).on("scroll", function(){
-//     if ($(window).scrollTop() > 0) {
-//         $('.header').addClass('on')
-//     } else {
-//         $('.header').removeClass('on') 
-//     }
-// })
-
-$(window).on('scroll', function(){
-    var sct=$(window).scrollTop(); // 변수 - var, let, const
-    sct > 0 ? $('.header').addClass('on') : $('.header').removeClass('on') //삼항조건식
-    if(sct > 0) {
-        $('.header').addClass('on')
-    } else {
-        $('.header').removeClass('on')
-    }
-})
-
-
-
-$('.main_slider').slick({
-    arrows:false,
-    fade:true,
-    dots:true,
-    autoplay:true,
-    pauseOnHover:false,
-    pauseOnFocus:false,
-});
-
-
-
-
-
-
-
-//---------------------
-});
+    $('.mopen').on('click', function(){
+        $('nav').toggleClass('on')
+        $('.header .right').toggleClass('on')
+    })
+    
+    //--------------------------------------------------------------
+    $(window).on('scroll', function(){
+        var sct=$(window).scrollTop(); //var let const 
+        sct > 0 ? $('.header').addClass('on') : $('.header').removeClass('on'); // 삼항조건식
+        // if(sct > 0) {
+        //     $('.header').addClass('on')
+        // } else {
+        //     $('.header').removeClass('on')  
+        // }
+    });
+    
+    
+    
+    
+    
+    $('.main_slider').slick({
+        arrows:false,
+        fade:true,
+        dots:true,
+        autoplay:true,
+        pauseOnHover:false,
+        pauseOnFocus:false,
+    });
+    
+    //--------------------------------------------------------------
+    });
+    
